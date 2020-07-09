92dc549c322b5ef68bc6e68784a294b4db84be70
DienF <68037919+DienF@users.noreply.github.com>
2020-07-09 14:56:59 +1100

    Mise à jour main.blocks, main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-input.onButtonPressed(Button.A, function () {
-    while (nb < 11) {
+def on_button_pressed_a():
+    global nb
+    if nb < 10:
         nb += 1
-        basic.showNumber(nb)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    break;
-continue;
-})
-let nb = 0
+        basic.show_number(nb)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global nb
+    if nb >= 10:
+        pass
+    else:
+        nb += 1
+        basic.show_number(nb)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+nb = 0
 nb = 1
-basic.showNumber(nb)
-basic.forever(function () {
-	
-})
+basic.show_number(nb)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
